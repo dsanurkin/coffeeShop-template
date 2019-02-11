b2d59b5fa73e4517c9a6cb08baff937621ce7f34
dsanurkin <ds.anurkin@yandex.ru>
2019-02-11 15:23:22 +0300

    first

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-$(document).ready(function() {
-    $('#multiscroll').multiscroll({
-        anchors: ['first', 'second', 'third'],
-        menu: '#menu',
-        navigation: true,
-        navigationTooltips: ['One', 'Two', 'Three']
-    });
+// $(document).ready(function() {
+
+//     $('.section-2').click(function(){
+//         $('#section-1').hide();
+//         $('#section-2').show();
+//     });    
+//     $('.section-3').click(function(){
+//         $('#section-1','#section-2','#section-4','#section-5').css('display','none');
+//         $('#section-3').css('display','flex');
+//     });    
+//     $('.section-4').click(function(){
+//         $('#section-1').css('display','none');
+//         $('#section-2').css('display','flex');
+//     });    
+//     $('.section-5').click(function(){
+//         $('#section-1').css('display','none');
+//         $('#section-2').css('display','flex');
+//     });    
     
-});
+// });
