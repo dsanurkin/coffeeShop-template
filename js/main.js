9958e087a39423569ffe680e0fabf6cfe267daa0
dsanurkin <ds.anurkin@yandex.ru>
2019-02-18 16:45:57 +0300

    Final

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
   $(function () { objectFitImages() });
+  jQuery('.burger').click(function(e) {
+    e.preventDefault();
+    jQuery('.menu-icon').toggleClass('opened');
+    $('.sidebar').toggleClass('sidebar-opened');
+  });
+  (function($){
+    $(window).on("load",function(){
+        $(".sidebar").mCustomScrollbar({
+          theme:"light-thick",
+          setWidth: true
+        });
+    });
+})(jQuery);
 });
